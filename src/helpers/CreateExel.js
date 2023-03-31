28c12b38334f560780e4944df9f8a89d0c0f8384
Urielr2 <erickurr0312@gmail.com>
2023-03-30 20:43:18 -0600

    ADD: estable

--- src/helpers/CreateExel.js
+++ src/helpers/CreateExel.js
@@ -1,13 +1,11 @@
 import XLSX from 'xlsx';
 const RNFS = require('react-native-fs');
 
-
-
 export const exportDataToExcel = (text) => {
       
         
     // Created Sample data
-    let sample_data_to_export = [{nombre: 'marin', tel: 1111111},{nombre:'mario', tel: 467456780}]; //aqui vamos a recibir la dat del lector de QRs
+    let sample_data_to_export = [{nombre: 'marcus', tel: 5050505},{nombre:'mimosa', tel: 56987567}]; //aqui vamos a recibir la dat del lector de QRs
 
     let wb = XLSX.utils.book_new();
     let ws = XLSX.utils.json_to_sheet(sample_data_to_export)    
@@ -18,7 +16,7 @@ export const exportDataToExcel = (text) => {
     // Write generated excel to Storage
     RNFS.writeFile(RNFS.DownloadDirectoryPath + `/${text}.csv`, wbout, 'ascii')
     .then((r)=>{
-     console.log('Success');
+     console.log('Asistencia tomada, su archivo se descargo correctamente');
     }).catch((e)=>{
       console.log('Error', e);
     });
